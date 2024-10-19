7e0a39d7a5dde64eb4d2f3cf0b6c2304f7333e36
Weronika <weronika.stefanska86@gmail.com>
2024-10-19 16:34:23 +0200

    Inital commit

---
+++ app/src/main/java/com/example/quizlista/MainActivity.kt
@@ -0,0 +1,210 @@
+package com.example.quizlista
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.lang.Thread.sleep
+
+data class Question(
+    val questionId: Int,
+    val questionText: String,
+    val answers: List<Answer>
+)
+
+data class Answer(
+    val answerId: Int,
+    val answerText: String
+)
+
+val questions = listOf(
+    Question(
+        questionId = 1,
+        questionText = "W jakim stanie skupienia substancje mają największą energię wewnętrzną?",
+        answers = listOf(
+            Answer(1, "Gazowym"),
+            Answer(2, "Ciekłym"),
+            Answer(3, "Stałym"),
+            Answer(4, "Wszystkie mają taką samą energię")
+        )
+    ),
+    Question(
+        questionId = 2,
+        questionText = "Co wyraża zasada zachowania energii?",
+        answers = listOf(
+            Answer(1, "Całkowita energia układu pozostaje stała"),
+            Answer(2, "Energia zależy od masy ciała"),
+            Answer(3, "Energia nie zmienia swojej formy"),
+            Answer(4, "Energia może być tworzona i niszczona")
+        )
+    ),
+    Question(
+        questionId = 3,
+        questionText = "Jakie jest twoje imię?",
+        answers = listOf(
+            Answer(1, "Jan"),
+            Answer(2, "Anna"),
+            Answer(3, "Piotr"),
+            Answer(4, "Krzysztof")
+        )
+    ),
+    Question(
+        questionId = 4,
+        questionText = "Co się stanie z częstotliwością fali, gdy długość fali się zmniejszy?",
+        answers = listOf(
+            Answer(1, "Częstotliwość wzrośnie"),
+            Answer(2, "Częstotliwość zmaleje"),
+            Answer(3, "Nie zmieni się"),
+            Answer(4, "Fala zaniknie")
+        )
+    ),
+    Question(
+        questionId = 5,
+        questionText = "Jaką jednostkę ma ładunek elektryczny?",
+        answers = listOf(
+            Answer(1, "C"),
+            Answer(2, "A"),
+            Answer(3, "J"),
+            Answer(4, "W")
+        )
+    ),
+    Question(
+        questionId = 6,
+        questionText = "Który z materiałów najlepiej przewodzi ciepło?",
+        answers = listOf(
+            Answer(1, "Aluminium"),
+            Answer(2, "Szkło"),
+            Answer(3, "Plastik"),
+            Answer(4, "Drewno")
+        )
+    ),
+    Question(
+        questionId = 7,
+        questionText = "Co jest podstawową przyczyną ruchu planet wokół Słońca?",
+        answers = listOf(
+            Answer(1, "Siła grawitacji"),
+            Answer(2, "Tarcie"),
+            Answer(3, "Ciśnienie"),
+            Answer(4, "Siła magnetyczna")
+        )
+    ),
+    Question(
+        questionId = 8,
+        questionText = "Jaka jest prędkość światła w próżni?",
+        answers = listOf(
+            Answer(1, "300 000 km/s"),
+            Answer(2, "100 000 km/s"),
+            Answer(3, "150 000 km/s"),
+            Answer(4, "500 000 km/s")
+        )
+    ),
+    Question(
+        questionId = 9,
+        questionText = "Co się dzieje z ciałem o masie 2 kg, jeśli działa na nie siła 10 N?",
+        answers = listOf(
+            Answer(1, "Przyspiesza o 5 m/s²"),
+            Answer(2, "Przyspiesza o 10 m/s²"),
+            Answer(3, "Nic się nie dzieje"),
+            Answer(4, "Przyspiesza o 2 m/s²")
+        )
+    ),
+    Question(
+        questionId = 10,
+        questionText = "Gdzie znajduje się Uniwersytet Wrocławski?",
+        answers = listOf(
+            Answer(1, "Wrocław"),
+            Answer(2, "Kraków"),
+            Answer(3, "Warszawa"),
+            Answer(4, "Poznań")
+        )
+    )
+)
+
+class MainActivity : AppCompatActivity() {
+
+    private val showQuestion: TextView by lazy{findViewById(R.id.qcontent)}
+    private val showQuestionCount: TextView by lazy{findViewById(R.id.pytanie)}
+
+    private var numQuestion = 0
+
+    private val r1: RadioButton by lazy{findViewById(R.id.radioButton1)}
+    private val r2: RadioButton by lazy{findViewById(R.id.radioButton2)}
+    private val r3: RadioButton by lazy{findViewById(R.id.radioButton3)}
+    private val r4: RadioButton by lazy{findViewById(R.id.radioButton4)}
+
+    private val submitButton: Button by lazy{findViewById(R.id.submit)}
+    private val radioGroup: RadioGroup by lazy{findViewById((R.id.rg))}
+    private val viewScore: TextView by lazy{findViewById(R.id.score)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var showQuestionCountText = "Pytanie ${numQuestion+1}/10"
+        val randQuest = questions.shuffled()
+        var randAnswer = randQuest[numQuestion].answers.shuffled()
+        var score = 0
+
+        fun set() {
+            r1.text = randAnswer[0].answerText
+            r1.tag = randAnswer[0].answerId
+            r2.text = randAnswer[1].answerText
+            r2.tag = randAnswer[1].answerId
+            r3.text = randAnswer[2].answerText
+            r3.tag = randAnswer[2].answerId
+            r4.text = randAnswer[3].answerText
+            r4.tag = randAnswer[3].answerId
+        }
+
+        set()
+
+        showQuestion.text = randQuest[numQuestion].questionText
+        showQuestionCount.text = showQuestionCountText
+
+        submitButton.setOnClickListener {
+            if (radioGroup.checkedRadioButtonId != -1){
+                val selectedRadioButton = findViewById<RadioButton>(radioGroup.checkedRadioButtonId)
+                val selectedId = selectedRadioButton.tag as Int
+
+                if (selectedId == 1) {
+                    score++
+                }
+
+                if (numQuestion < 9) {
+                    numQuestion++
+
+
+                    showQuestion.text = randQuest[numQuestion].questionText
+                    showQuestionCountText = "Pytanie ${numQuestion + 1}/10"
+                    showQuestionCount.text = showQuestionCountText
+
+                    randAnswer = randQuest[numQuestion].answers.shuffled()
+                    set()
+
+                    radioGroup.clearCheck()
+                } else {
+                    viewScore.text = "Wynik: ${score*10} punktów."
+                    viewScore.visibility = View.VISIBLE
+                    sleep(5000)
+
+                }
+            } else {
+                Toast.makeText(this, "Wybierz odpowiedź!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
